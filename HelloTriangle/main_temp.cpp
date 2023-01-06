ab196ff9b590d5e57c21d3f8ac5e8fbabdcf105e
Masoud Darvishian <masoud.darvishian@etermax.de>
2023-01-06 08:35:11 +0100

    practice: create an empty window

--- HelloTriangle/main_temp.cpp
+++ HelloTriangle/main_temp.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
-void frame_buffer_size_callback(GLFWwindow* window, int width, int height);
+void processTheInput(GLFWwindow*);
+void displayMessage(std::string);
 
 int main() {
-
+	
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(700, 500, "LearnOpenGL", NULL, NULL);
-	if (!window)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif 
+
+    GLFWwindow* window = glfwCreateWindow(600, 400, "Learn OpenGL", NULL, NULL);
+	if (window == NULL) {
+		displayMessage("Failed to create window!");
 		glfwTerminate();
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "failed to initialize GLAD" << std::endl;
-		glfwTerminate();
+		displayMessage("Failed to initialize glad!");
 		return -1;
 	}
 
 	while (!glfwWindowShouldClose(window)) {
-		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-			glfwSetWindowShouldClose(window, true);
-		}
+		processTheInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -42,10 +42,14 @@ int main() {
 	}
 
 	glfwTerminate();
-
 	return 0;
 }
 
-void frame_buffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
+void displayMessage(std::string msg) {
+	std::cout << msg << std::endl;
+}
+
+void processTheInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
